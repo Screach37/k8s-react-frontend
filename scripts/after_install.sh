ad9b152a524f47d5baa09206b71e1a6070c969cc
Ravi sarswat <rmanan@192.168.1.15>
2023-07-31 18:28:25 +0530

    updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd ${DESTINATION_PATH}
+cd '${DESTINATION_PATH}'
 
 #_Remove_Unused_Code
 rm -rf node_modules
@@ -10,4 +10,4 @@ rm -rf build
 #Install_node_modules_&_Make_React_Build
 npm  install
 npm run build
-cp -rf ${DESTINATION_PATH}/build/* ${DOCUMENT_ROOT}
+cp -rf '${DESTINATION_PATH}'/build/* '${DOCUMENT_ROOT}'
