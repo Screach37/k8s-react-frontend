ace081a8f6de3bac26335de1b61ff7b8685953b4
CTO at RNS <129362492+rnscto@users.noreply.github.com>
2023-08-01 12:53:12 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd '${DESTINATION_PATH}'
+cd "${DESTINATION_PATH}"
 
 # Debugging statements
 echo "Current working directory: $(pwd)"
 echo "Is /home/ec2-user/demo directory present? $(ls -ld /home/ec2-user/demo)"
-
 
 #_Remove_Unused_Code
 rm -rf node_modules
 rm -rf build
 
 #Install_node_modules_&_Make_React_Build
-npm  install
+npm install
 npm run build
-cp -rf '${DESTINATION_PATH}'/build/* '${DOCUMENT_ROOT}'
+cp -rf "${DESTINATION_PATH}/build/" "${DOCUMENT_ROOT}"
