e73fc8c79a14621cc0911f3f9a1b974306c1ee00
CTO at RNS <129362492+rnscto@users.noreply.github.com>
2023-08-01 13:02:28 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd '${DESTINATION_PATH}'
+cd "${DESTINATION_PATH}"
 
 #_Delete_Old_PM2_Service
 #sudo pm2 delete Frontend
 #sudo pm2 start server.js --name Frontend
-sudo pm2 delete '${SERVICE_NAME}'
-sudo pm2 start server.js --name '${SERVICE_NAME}'
+sudo pm2 delete "${SERVICE_NAME}"
+sudo pm2 start server.js --name "${SERVICE_NAME}"
