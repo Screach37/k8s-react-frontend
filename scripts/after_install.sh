ea225ed52a0238d2939c35d141732e96fffc5afd
Ravi sarswat <rmanan@192.168.0.191>
2023-08-02 12:37:45 +0530

    hello

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # Retrieve the values from AWS Systems Manager Parameter Store
-#DESTINATION_PATH=$(aws ssm get-parameter --name "/MyApp/DESTINATION_PATH" --query "Parameter.Value" --output text)
-#DOCUMENT_ROOT=$(aws ssm get-parameter --name "/MyApp/DOCUMENT_ROOT" --query "Parameter.Value" --output text)
-
-#source ./config.sh
-mkdir -p "${DOCUMENT_ROOT}"
-cd "${DOCUMENT_ROOT}"
+DESTINATION_PATH=$(aws ssm get-parameter --name "/MyApp/DESTINATION_PATH" --query "Parameter.Value" --output text)
+DOCUMENT_ROOT=$(aws ssm get-parameter --name "/MyApp/DOCUMENT_ROOT" --query "Parameter.Value" --output text)
 
 # Use the retrieved variables as needed in your script
 cd "${DESTINATION_PATH}"
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Retrieve the values from AWS Systems Manager Parameter Store
-#DESTINATION_PATH=$(aws ssm get-parameter --name "/MyApp/DESTINATION_PATH" --query "Parameter.Value" --output text)
-#DOCUMENT_ROOT=$(aws ssm get-parameter --name "/MyApp/DOCUMENT_ROOT" --query "Parameter.Value" --output text)
-#SERVICE_NAME=$(aws ssm get-parameter --name "/MyApp/SERVICE_NAME" --query "Parameter.Value" --output text)
-
-#source ./config.sh
+DESTINATION_PATH=$(aws ssm get-parameter --name "/MyApp/DESTINATION_PATH" --query "Parameter.Value" --output text)
+DOCUMENT_ROOT=$(aws ssm get-parameter --name "/MyApp/DOCUMENT_ROOT" --query "Parameter.Value" --output text)
+SERVICE_NAME=$(aws ssm get-parameter --name "/MyApp/SERVICE_NAME" --query "Parameter.Value" --output text)
 
 
 # Use the retrieved variables as needed in your script
